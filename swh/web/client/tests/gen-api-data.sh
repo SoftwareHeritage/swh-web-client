29cd8ab2d04d0718564cdc360ef177ba84bee42a
Stefano Zacchiroli <zack@upsilon.cc>
2020-03-18 20:10:49 +0100

    web client tests: add missing copyright headers

--- swh/web/client/tests/gen-api-data.sh
+++ swh/web/client/tests/gen-api-data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Copyright (C) 2020  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 # Depends: curl, jq
 
 API_URL_real="https://archive.softwareheritage.org/api/1"
