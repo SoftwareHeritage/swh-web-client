203712188188223e492b8dd63ac251a2d99e9e20
Antoine Lambert <anlambert@softwareheritage.org>
2023-05-11 12:00:59 +0200

    cli: Fix AttributeError after config_basepath removal
    That function from swh.core.config was removed in swh/devel/swh-core@2663c0a4.

--- swh/web/client/cli.py
+++ swh/web/client/cli.py
@@ -50,7 +50,7 @@ def web(ctx: Context, config_file: str):
         config_file = DEFAULT_CONFIG_PATH
 
     try:
-        conf = config.read_raw_config(config.config_basepath(config_file))
+        conf = config.read_raw_config(config_file)
         if not conf:
             raise ValueError(f"Cannot parse configuration file: {config_file}")
 
